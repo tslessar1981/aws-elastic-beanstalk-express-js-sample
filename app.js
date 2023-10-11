aeb2e758f44293eb97b2490a1c0952e21502035e
tslessar <96509241+tslessar@users.noreply.github.com>
2023-10-11 11:40:23 +1000

    change message #2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What a Change'));
+app.get('/', (req, res) => res.send('Setting up home PC for Git'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
